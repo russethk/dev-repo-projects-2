ce755c6c3513e0ab324b69d33a39d86231370ade
krusseth-designs <66326669+krusseth-designs@users.noreply.github.com>
2023-04-25 10:29:17 -0400

    Add files via upload

---
+++ week0_HTML_CSS/javascript-intro/script.js
@@ -0,0 +1,25 @@
+if(Math.random() > .5) { 
+    console.log('Over 0.5'); 
+} else { 
+    console.log('Under 0.5'); 
+}
+
+
+/* 
+let name = 'Karen Russeth';
+
+// Parse the string value to and integer, then assign the integer value to the variable 'dayOfBirth' 
+let dayOfBirth = parseInt('5');
+
+// value of variable 'name' changed from original value to new assigned value
+let name = 'Kevin Orol';
+
+// Create a prompt to ask for input of a string value.  Assign the value of the entered strring to the variable 'color'
+let color = prompt("What is your favorite color?");   
+console.log(color); 
+
+// String with mixed quotation marks
+"The young boy said \"I don\'t have my driver\'s license yet!"
+*/
+
+
