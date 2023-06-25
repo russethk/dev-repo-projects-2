bdd50edc7060b3a4fab2ca5e8eea91283f6d4696
Karen Russeth <66326669+krusseth-designs@users.noreply.github.com>
2023-06-25 09:20:04 -0400

    Add files via upload

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -74,6 +74,8 @@ function placeInTable(y, x) {
   const piece = document.createElement('div');
   piece.classList.add('piece');
   piece.classList.add(`p${currPlayer}`);
+  piece.style.top = -50 * (y + 2);
+  
   const spot = document.getElementById(`${y}-${x}`);
   spot.append(piece);
 }
